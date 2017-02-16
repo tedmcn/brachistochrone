8799559146e85ae42eaba19481b590966e934e40
tedmcn <tedmcnu@gmail.com>
2017-02-16 17:17:34 +0000

    toDegree

--- src/vectorobject.cpp
+++ src/vectorobject.cpp
@@ -24,6 +24,22 @@ protected:
 }
 
 //Averages two different vectors
+//
+//The average of {0,1,0} and {1,0,0} (a vector going straigh up and straight 
+//on the x axis) should be {1,1,0} because it is going in between the straight
+//up and straight to the side vectors, resulting in a x=y vector.
 Vectorobject Vectorobject::average(Vectorobject v){
     
+}
+
+//Returns the vector represented in degrees as 2 floats from 0-360
+//
+//The degree of a vector of {0,0,0} should be null because it is not pointing
+//anywhere. 
+//
+//The first float representing degree should represent x and z axies 
+//(ie the horozontal ring around a sphere) and the second float should
+//represent the y and z axies (ie around the sphere vertically)
+float* Vectorobject::toDegree(){
+
 }
